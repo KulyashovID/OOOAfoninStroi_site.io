975578afc0846eda4464767ae6ac94efee11a37b
Илья Куляшов <I.Kulyashov@Buro.local>
2026-07-14 12:17:58 +0300

    начало работы с кнопками и +интерпритатор

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const button = document.getElementById("btn");
+const modal = document.getElementById("modal");
+const close = document.querySelector(".close");
 
-button.addEventListener("click", function(){
-    alert("Позже здесь появится подробная информация.");
+// Открыть окно
+button.addEventListener("click", function () {
+    modal.style.display = "flex";
+});
+
+// Закрыть по крестику
+close.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// Закрыть при клике вне окна
+window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 });
